d745b21aec1bc23f37bdd22ee3e28708a8f23205
celecigo <cimapublicidad.imp@gmail.com>
2026-05-17 10:31:01 -0500

    cambio mysql pool

--- app/db.js
+++ app/db.js
@@ -2,23 +2,16 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log(process.env.DB_HOST);
-const connection = mysql.createConnection({
+
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
-
-    if (err) {
-        console.error("Error conectando a MySQL:", err);
-        return;
-    }
-
-    console.log("Conectado a MySQL Railway");
-});
-
-export default connection;
+export default pool.promise();
