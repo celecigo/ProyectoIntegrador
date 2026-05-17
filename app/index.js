e2ee98d0ebdb2aef7984391887c3b10727bd7a4b
celecigo <cimapublicidad.imp@gmail.com>
2026-05-17 10:06:57 -0500

    configuracion vercel

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ import bcrypt from "bcrypt";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
